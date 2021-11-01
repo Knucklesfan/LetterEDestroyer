af2003b590598bedcebf294af72cce5a167eef8f
Knucklesfan <bigfootreserchersoftexas@gmail.com>
2021-10-31 22:41:36 -0500

    addd th actual script to th rpository

---
+++ lttrdstroyingscript.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         I hate the letter E
+// @namespace    http://tampermonkey.net/
+// @version      6.9
+// @description  I hate the letter E more than I've ever hated anything on the face of this earth, just merely mentioning the letter E fills me with an indescribable hatred of the letter. I've had a deep seated hatred for the letter E since childhood and now that it's finally time that it has come that I must mascare this god forsaken letter for I hate it so much.
+// @author       Knuxfan the destroyer
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+
+//this script is a joke btw
+String.prototype.replaceAll = function(str1, str2, ignore) {
+    return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
+}
+function textNodesUnder(el){
+  var n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
+  while(n=walk.nextNode()) a.push(n);
+  return a;
+}
+
+function main(){
+    var all = textNodesUnder(document.body);
+
+    for (var i=0, max=all.length; i < max; i++) {
+        if(all[i].textContent != undefined) {
+            if(all[i].textContent.includes("e")) {
+                  var str = all[i].textContent.replaceAll("e","");
+                  all[i].textContent = str;
+            }
+        }
+    }
+}
+
+main();
